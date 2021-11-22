955fe4510337b18d571db48ffe077c685402a85f
lmerwin <92190697+lmerwin@users.noreply.github.com>
2021-11-21 21:03:13 -0600

    Update main.java
    InfixToPostfix creating tree straight from the infix expression.  Getting error in function newNode that creates new nodes for tree. Not sure if algorithm for InfixToPostfix is correct because I cant' figure out how to get it to run without errors to test it.

--- CPT-287-Pr3/main/main.java
+++ CPT-287-Pr3/main/main.java
@@ -5,39 +5,130 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class main {
-
+public class Main {
 	public static void main(String[] args) throws IOException {
 		//Open input file, scanner, output file, and writer
-		FileInputStream inputFile = new FileInputStream("E:/downloads/input.txt");
+		FileInputStream inputFile = new FileInputStream("input.txt");
 		Scanner scanner = new Scanner(inputFile);
 		
 		//Scan input file and call the functions to calculate expressions from input file
 		while (scanner.hasNextLine()) {
 			String infixExp = scanner.nextLine();
 			//insert function for converting infix to postfix
-			String postfix = infixToPostfix(infixExp);
+			BTNode expTree = infixToPostfix(infixExp);
+			postorderTraverse(expTree);
+			
 			//insert function to evaluate postfix expression and print to screen
-			System.out.println(evaluatePostfix(postfix));
+
 		}
 		scanner.close();
 		inputFile.close();
+	}
+	
+	
+	/**
+	 * Turns an infix expression into a postfix expression
+	 * @param infixExp: original infix expression
+	 * @return: postfix expression
+	 */
+	public static BTNode infixToPostfix(String infixExp) {
+		// Stack to hold digits
+		Stack<BTNode> nodes = new Stack<>();
+		// Stack to hold operators
+		Stack<String> operators = new Stack<>();
+		BTNode top, top1, top2;
 		
+		Scanner scanner = new Scanner(infixExp);
+		
+		while (scanner.hasNext()) {
+			String token = scanner.next();
+			if (token.equals("(")) {
+				// push '(' to operators stack
+				operators.add(token);
+			}
+			else if (Character.isDigit(token.charAt(0))) {
+				// push numbers to nodes stack as node
+				top = newNode(token);
+				nodes.add(top);
+			}
+			else if (token.equals(")")) {
+				while (!operators.isEmpty() && operators.peek() != "(") {
+					top = newNode(operators.peek());
+					operators.pop();
+					top1 = nodes.peek();
+					nodes.pop();
+					top2 = nodes.peek();
+					nodes.pop();
+					top.left = top2;
+					top.right = top1;
+					nodes.add(top);
+				}
+				operators.pop();
+			}
+			else {
+				while (!operators.isEmpty() && !operators.peek().equals("(") && precedence(token) <= precedence(operators.peek())) {
+					// Get and remove top element from operators stack
+					top = newNode(operators.peek());
+					operators.pop();
+					// Get and remove the top element from nodes stack
+					top1 = nodes.peek();
+					nodes.pop();
+					// Get and remove the next element from nodes stack
+					top2 = nodes.peek();
+					nodes.pop();
+					// Update the tree
+					top.left = top2;
+					top.right = top1;
+					// Push the operator to the nodes stack
+					nodes.add(top);
+				}
+				// Push token to operator stack
+				operators.push(token);
+			}
+		}
+		top = nodes.peek();
+		return top;
+	}
+	
+	
+	// Binary Tree Node
+	public class BTNode<T> {
+		// Data Field
+		public T data;
+		public BTNode<T> left, right;
+		
+		// Constructor
+		public BTNode(T value) { data = value; }
+		
+		public BTNode(T value, BTNode<T> leftChild, BTNode<T> rightChild) {
+			data = value;
+			left = leftChild;
+			right = rightChild;
+		}
 		
 	}
 	
 	
-	
-	
-	public static int evaluatePostfix (String str) {
+	// Function to create new node
+	static BTNode newNode(String s) {
+		BTNode<String> node = null;
+		node.data = s;
+		node.left = node.right = null;
+		return node;
 		
 	}
 	
-	
-	public static String infixToPostfix(String infixExp) {
-		
+	/**
+	 * Postorder traverse a binary tree
+	 * @param root: root node of binary tree
+	 */
+	public static <T> void postorderTraverse(BTNode<T> root) {
+		if (root != null) {
+			postorderTraverse(root.left);
+			postorderTraverse(root.right);
+			System.out.print(root.data.toString() + ' ');
+		}
 	}
-	
 	
 	
 	/** Returns the precedence of an operator.
@@ -58,6 +149,11 @@ public static int precedence(String operator) {
 	throw new IllegalArgumentException(String.format("Operator %s is not a valid operator.", operator));
 }
 	
+	/**
+	 * Formats spaces into input 
+	 * @param original: original string
+	 * @return string with spaces between operands and operators
+	 */
 	public static String format(String original) {
 		// convert to character array
 		char[] arr = original.toCharArray();
@@ -106,14 +202,4 @@ public static int precedence(String operator) {
 		}
 		return spaced.toString();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
